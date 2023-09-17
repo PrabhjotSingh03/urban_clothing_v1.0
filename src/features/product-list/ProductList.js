eae2d20ff57c57c3967e4c083451fd8f84f5541e
Prabhjot Singh Sains <prabhjot.sains@gmail.com>
2023-09-17 03:02:27 -0400

    Added Static Pagination to the product list page

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -469,6 +469,16 @@ export default function ProductList() {
                       >
                         3
                       </a>
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        <span className="sr-only">Next</span>
+                        <ChevronRightIcon
+                          className="h-5 w-5"
+                          aria-hidden="true"
+                        />
+                      </a>
                     </nav>
                   </div>
                 </div>
