2c8003bf4f40d86a051f87e3d1accb7289888cfe
Prabhjot Singh Sains <prabhjot.sains@gmail.com>
2023-09-24 21:41:04 -0400

    Fixed Category Selection

--- src/features/product-list/productAPI.js
+++ src/features/product-list/productAPI.js
@@ -1,22 +1,30 @@
 export function fetchAllProducts() {
   return new Promise(async(resolve) =>{
     const response = await fetch('http://localhost:8000/products')
-    const data = await response.json()
-    resolve({data})
+    const data = await response.json();
+    resolve({data});
   }
   );
 }
 
-export function fetchProductsByFilters(filter) {
+export function fetchProductsByFilters(filter,sort) {
   let queryString = '';
   for(let key in filter){
-    queryString += `${key}=${filter[key]}&`
+    const categoryValues = filter[key];
+    if(categoryValues.length){
+      const lastCategoryValue = categoryValues[categoryValues.length-1];
+      queryString += `${key}=${lastCategoryValue}&`
+    }
+  }
+
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
   }
 
   return new Promise(async(resolve) =>{
-    const response = await fetch('http://localhost:8000/products?'+queryString)
-    const data = await response.json()
-    resolve({data})
+    const response = await fetch('http://localhost:8000/products?'+queryString);
+    const data = await response.json();
+    resolve({data});
   }
   );
 }
